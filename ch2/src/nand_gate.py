6128f0281beff061216363eb6b4c3be9fe400cee
Takumi Kiura <kiutaku0212@outlook.jp>
2026-05-08 14:34:17 +0900

    add: create nand_gate and set defaulut parameter correctly

---
+++ ch2/src/nand_gate.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+DEFAULT_W = np.array([-0.5, -0.5])
+DEFAULT_B = 0.7
+
+def set_param():
+    print("input parameter w1: ")
+    w1 = float(input())
+    print("input parameter w2: ")
+    w2 = float(input())
+    w = np.array([w1, w2])
+
+    print("input parameter b: ")
+    b = float(input())
+
+    return w, b
+
+def nand_gate(x1, x2, w, b):
+    x = np.array([x1, x2])
+
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+if __name__ == "__main__":
+    # w, b = set_param()
+    for x_array in [(0, 0), (1, 0), (0, 1), (1, 1)]:
+        y = nand_gate(x_array[0], x_array[1], DEFAULT_W, DEFAULT_B)  # use default parameters
+        print(str(x_array) + " -> " + str(y))
